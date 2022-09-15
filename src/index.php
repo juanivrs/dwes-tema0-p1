0481dbc4593438fdb62ad8348cf5bd2326203e0e
Juan Ivars <juanivarsmartinez@gmail.com>
2022-09-15 19:43:07 +0200

    Segundo commit

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-i">
+
+    <title>Hola Mundo</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        echo "¡Hola Mundo!";
+
+    ?>
+
+</body>
+
+</html>
